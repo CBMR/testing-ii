335bab7621ec7785f1e30374c419bbe0f2b4c63d
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-04 18:04:44 -0500

    passed the dashboard component to the app component

--- baseball_staduim/src/App.js
+++ baseball_staduim/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import {} from './Display'
-import {} from './Dashboard'
+import Dashboard from './Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Dashboard />
       </div>
     );
   }
